56e2d041647806f73516b0a3b909813d535613bf
yonghyun123 <model9960@naver.com>
2018-01-04 23:33:46 +0900

    java lang function and recursion

---
+++ src/chapter06/Question06.java
@@ -0,0 +1,14 @@
+package chapter06;
+
+public class Question06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(square(5));
+	}
+	public static int square(int num){
+		if(num == 1) return 1;
+		return 2 * square(num-1);
+	}
+
+}
