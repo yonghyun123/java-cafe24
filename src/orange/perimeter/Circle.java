b4cfc7524fb406f9dc9b6d78f6bd78e3c78edf09
yonghyun123 <model9960@naver.com>
2018-01-05 12:39:44 +0900

    complete chapter08 definition of package, import concept

---
+++ src/orange/perimeter/Circle.java
@@ -0,0 +1,14 @@
+package orange.perimeter;
+
+public class Circle {
+	double rad;
+	final double PI;
+	
+	public Circle(double r){
+		rad = r;
+		PI = 3.14;
+	}
+	public double getPerimeter(){
+		return (rad*2)*PI;
+	}
+}
